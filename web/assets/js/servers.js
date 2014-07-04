27ffb15be0621b171f5dadacf68ad726b0641cc2
J7mbo <jm_cj@hotmail.com>
2014-07-04 12:23:04 +0100

    Removed JS for 'add client'. Added reload on delete client. Flash messages server-side handle any errors, with a page refresh.

--- web/assets/js/servers.js
+++ web/assets/js/servers.js
@@ -5,17 +5,6 @@
         $button.addEventListener('click', onDeleteClientButtonClick, false);
     });
 
-    $.each($('.add-server-btn'), function(key, $button) {
-        $button.addEventListener('click', onAddNewServerButtonClick, false);
-    });
-
-    /** **/
-    function onAddNewServerButtonClick(e) {
-        e.preventDefault();
-
-
-    }
-
     /** Delete a client **/
     function onDeleteClientButtonClick(e) {
         e.preventDefault();
@@ -33,14 +22,9 @@
                 type:     'POST',
                 url:      removeClientUrl,
                 data:     { id: deleteId }
-            }).done(function(data) {
-                console.log(data);
-                // @todo Add 'undo' link, bar etc
-            }).fail(function(jqXHR, textStatus, errorThrown) {
-                console.log(errorThrown);
-                // @todo Notify something went wrong and to refresh / try again
             }).always(function() {
-                $(this).prop('disabled', false);
+                /** We'll show any errors via the flashbag server-side **/
+                location.reload();
             });
         }
     }
